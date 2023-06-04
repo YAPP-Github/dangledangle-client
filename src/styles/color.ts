9e479d771f1be648abff2e9fd2e2d8f4938b654d
HaYeong <hy78733@gmail.com>
2023-06-05 01:46:06 +0900

    ✨ feat: color palette, Color 타입 정의

---
+++ src/styles/color.ts
@@ -0,0 +1,19 @@
+export const palette = {
+  white: 'white',
+  gray50: '#F8F8F8',
+  gray100: '#F3F3F3',
+  gray200: '#EDEDED',
+  gray300: '#D5D5D5',
+  gray400: '#B1B1B1',
+  gray500: '#909090',
+  gray600: '#6C6C6C',
+  gray700: '#464646',
+  gray800: '#333333',
+  gray900: '#222222',
+  primary100: '#D3F0EF',
+  primary200: '#9DF5F3',
+  primary300: '#00D9D4',
+  error: '#FF2121'
+};
+
+export type Color = keyof typeof palette;
--- src/styles/theme.css.ts
+++ src/styles/theme.css.ts
@@ -1,15 +1,12 @@
-import { createTheme, globalFontFace, globalStyle } from '@vanilla-extract/css';
-import color from './color.json';
+import { globalFontFace, globalStyle } from '@vanilla-extract/css';
 
+/* Global font */
 const pretendard = 'Pretendard Variable';
 globalFontFace(pretendard, {
   src: `url('https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.6/dist/web/variable/pretendardvariable-dynamic-subset.css')`
 });
 
 globalStyle('html, body', {
-  fontFamily: `${pretendard} -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;`
-});
-
-export const [themeClass, vars] = createTheme({
-  color
+  fontFamily: `${pretendard} -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;`,
+  fontStyle: 'normal'
 });
