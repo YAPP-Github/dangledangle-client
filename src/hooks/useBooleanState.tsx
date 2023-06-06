fc0aad75a5bdc224992a3ef8cca618e4ca2f06c8
Paperkeem <ahharlawhddl@gmail.com>
2023-06-06 19:56:02 +0900

    🚀 update: useBooleanState 배열로 반환

--- src/hooks/useBooleanState.tsx
+++ src/hooks/useBooleanState.tsx
@@ -1,11 +1,24 @@
 import { useState, useCallback } from 'react';
 
-export default function useBooleanState(initialValue: boolean = false) {
+export default function useBooleanState(initialValue = false) {
   const [isBooleanState, setBooleanState] = useState(initialValue);
+
+  const setBoolTrue = useCallback(() => {
+    setBooleanState(true);
+  }, []);
+
+  const setBoolFalse = useCallback(() => {
+    setBooleanState(false);
+  }, []);
 
   const toggleBooleanState = useCallback(() => {
     setBooleanState(!isBooleanState);
   }, [isBooleanState]);
 
-  return { isBooleanState, toggleBooleanState };
+  return [
+    isBooleanState,
+    setBoolTrue,
+    setBoolFalse,
+    toggleBooleanState
+  ] as const;
 }
