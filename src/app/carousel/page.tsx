1e07b48e6e5e7f3d817dbf95e36df4b8e81d6fd0
HaYeong <hy78733@gmail.com>
2023-06-07 23:52:58 +0900

    ✨ feat: checkBox 컴포넌트

--- src/app/carousel/page.tsx
+++ src/app/carousel/page.tsx
@@ -1,7 +1,11 @@
+'use client';
 import Carousel from '@/components/common/Carousel/Carousel';
 import * as styles from './styles.css';
+import CheckBox from '@/components/common/CheckBox/CheckBox';
+import { useState } from 'react';
 
 export default function CarouselTest() {
+  const [checked, setChecked] = useState(false);
   return (
     <div style={{ width: '100%' }}>
       <Carousel>
@@ -9,6 +13,8 @@ export default function CarouselTest() {
         <div className={styles.item} style={{ backgroundColor: 'orange' }} />
         <div className={styles.item} style={{ backgroundColor: 'yellow' }} />
       </Carousel>
+      <div style={{ marginBottom: '30px' }} />
+      <CheckBox value={checked} onClick={setChecked} label="체크박스" />
     </div>
   );
 }
