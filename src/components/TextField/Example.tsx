4f2d00df84bd820e48c2e4e5f0c26909da59607d
Sangjun-man <molung@icloud.com>
2023-06-08 18:03:58 +0900

    🚚 chore: textField 예시 페이지 생성

--- src/components/TextField/Example.tsx
+++ src/components/TextField/Example.tsx
@@ -4,15 +4,16 @@ import { useEffect, useRef, useState } from 'react';
 import TextField from './TextField';
 import { FieldValues, useForm } from 'react-hook-form';
 
-export default function TextFieldForm() {
+export default function TextFieldExample() {
   const { handleSubmit, control, register } = useForm();
   const [async, setStatus] = useState<{
     status: 'default' | 'active' | 'success' | 'error' | 'loading';
     message: string;
-  }>({ status: 'default', message: '12312312' });
+  }>({ status: 'default', message: '' });
   const ref = useRef<HTMLInputElement>(null);
   const ref2 = useRef<HTMLInputElement>(null);
   const ref3 = useRef<HTMLInputElement>(null);
+  const ref4 = useRef<HTMLInputElement>(null);
 
   const onSubmit = (data: FieldValues) => {
     console.log(data);
@@ -39,13 +40,39 @@ export default function TextFieldForm() {
         }}
       ></div>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-around'
+        }}
+      >
         <TextField
           label="기본 사용 폼"
           name="인풋1"
           placeholder="123123"
-          type="password"
+          message="아아아아"
           ref={ref} //useRef와 함께 사용
+        />
+
+        <TextField
+          label="기본 사용 폼 / 비밀번호"
+          name="인풋1"
+          placeholder="password"
+          type="password"
+          ref={ref2} //useRef와 함께 사용
+        />
+
+        <TextField
+          name="인풋3"
+          label="크기가 big인 폼"
+          size="big"
+          placeholder="123123"
+          defaultValue="12312"
+          validation={{ max: 5 }} //validation
+          ref={ref3}
         />
 
         <TextField
@@ -54,21 +81,11 @@ export default function TextFieldForm() {
           placeholder="123123"
           status={async.status}
           message={async.message} //useEffect에서 비동기 테스트,
-          ref={ref2}
-        />
-        <TextField
-          name="인풋3"
-          label="크기가 big인 폼"
-          size="big"
-          placeholder="123123"
-          defaultValue="12312"
-          validation={{ max: 5 }} //validation
-          type="password"
-          ref={ref3}
+          ref={ref4}
         />
 
         <TextField
-          label="react-form-hook과 함께 사용하는 폼 1"
+          label="react-form-hook과 함께 사용하는 폼 1, input 문자 수 / 인풋4"
           placeholder="123123"
           validation={{ max: 10 }}
           message="ㅁㄴㅇㄹ"
@@ -76,7 +93,7 @@ export default function TextFieldForm() {
           {...register('인풋4')} //react-form-hook과 함께 사용
         />
         <TextField
-          label="react-form-hook과 함께 사용하는 폼 2"
+          label="react-form-hook과 함께 사용하는 폼 2, 이메일 / 인풋5"
           placeholder="123123"
           validation={{ email: true }}
           message="ㅁㄴㅇㄹ"
