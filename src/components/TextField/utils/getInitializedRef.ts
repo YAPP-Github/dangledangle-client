9c0fe30af2b5b65ebed768444c3699c9c6c8f9c3
Sangjun-man <molung@icloud.com>
2023-06-10 15:36:14 +0900

    🔥 remove: 소문자로 된 컴포넌트 폴더 삭제

--- src/components/TextField/utils/getInitializedRef.ts
+++
@@ -1,8 +0,0 @@
-const getInitializedRef = <T>(ref: { current: T | null }) => {
-  if (ref.current === null) {
-    throw new Error('ref가 정의되지 않았습니다.');
-  }
-  return ref.current;
-};
-
-export default getInitializedRef;
