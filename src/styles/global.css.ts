8e6a70e24e94888271071c292c19ddb4325cd122
HaYeong <hy78733@gmail.com>
2023-06-14 14:43:00 +0900

    ✨ feat: global style에 `list-style: none` 추가

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,4 +1,6 @@
+import { globalStyle } from '@vanilla-extract/css';
 import 'vanilla-reset';
 
+globalStyle('ul, ol, li', { listStyle: 'none' });
 export const BREAK_POINT = 600;
 export const GLOBAL_PADDING_X = 20;
