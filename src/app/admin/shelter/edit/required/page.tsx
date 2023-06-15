caf06d276702d1f30562d97888631dc112c1617b
HaYeong <hy78733@gmail.com>
2023-06-15 18:07:39 +0900

    🚀 update: 필수 정보 수정 페이지 스타일, 주소 필드 추가

--- src/app/admin/shelter/edit/required/page.tsx
+++ src/app/admin/shelter/edit/required/page.tsx
@@ -2,11 +2,14 @@
 import Button from '@/components/common/Button/Button';
 import TextArea from '@/components/common/TextArea/TextArea';
 import TextField from '@/components/common/TextField/TextField';
+import AddressSearchBar from '@/components/shelter-edit/AddressSearchBar/AddressSearchBar';
 import { formatPhone } from '@/utils/formatInputs';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useCallback } from 'react';
-import { useForm } from 'react-hook-form';
+import { FieldErrors, useForm } from 'react-hook-form';
 import * as yup from 'yup';
+import * as styles from './styles.css';
+import { Caption1 } from '@/components/common/Typography';
 
 type FormValues = {
   name: string;
@@ -18,7 +21,7 @@ type FormValues = {
 const schema: yup.ObjectSchema<Partial<FormValues>> = yup
   .object()
   .shape({
-    name: yup.string().required(),
+    name: yup.string().required('보호소 이름을 입력해주세요'),
     phone: yup
       .string()
       .required('연락처를 입력해주세요.')
@@ -45,21 +48,35 @@ export default function ShelterEditRequiredPage() {
     []
   );
 
+  const onSubmit = (data: FormValues | FieldErrors<FormValues>) =>
+    console.log(data);
+
   return (
-    <form>
-      <TextField label="보호소 이름" {...register('name')} />
-      <TextField
-        label="보호소 연락처"
-        {...register('phone')}
-        onChange={handlePhoneChange}
-      />
-      <TextArea
-        fixHeight="128px"
-        max={300}
-        label="보호소 소개 문구"
-        {...register('description')}
-      />
-      <Button>저장하기</Button>
+    <form onSubmit={handleSubmit(onSubmit, onSubmit)}>
+      <div className={styles.container}>
+        <TextField label="보호소 이름" {...register('name')} />
+        <TextField
+          label="보호소 연락처"
+          {...register('phone')}
+          onChange={handlePhoneChange}
+        />
+        <div>
+          <Caption1 element={'label'} color="gray600">
+            보호소 주소
+          </Caption1>
+          <AddressSearchBar />
+          <TextField {...register('address')} />
+        </div>
+        <TextArea
+          fixHeight="128px"
+          max={300}
+          label="보호소 소개 문구"
+          {...register('description')}
+        />
+      </div>
+      <Button className={styles.button} itemType="submit">
+        저장하기
+      </Button>
     </form>
   );
 }
