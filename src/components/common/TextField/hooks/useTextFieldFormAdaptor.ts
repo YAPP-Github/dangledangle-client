b0ade9ac023bfcedfc98a35a188e0b0a589be035
Sangjun-man <molung@icloud.com>
2023-06-17 00:32:42 +0900

    🔨 fix: useTextFieldAdaptor의 handelSubmit 동작방식 수정
    - 기본 validation을 건너뛰는게 아니라 errorState 조회 이전에 먼저 실행시키는 방식으로 수정

--- src/components/common/TextField/hooks/useTextFieldFormAdaptor.ts
+++ src/components/common/TextField/hooks/useTextFieldFormAdaptor.ts
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { SyntheticEvent } from 'react';
 import {
   FieldValue,
   UseFormHandleSubmit,
@@ -7,64 +7,61 @@ import {
 import { isEmptyObject } from '@/utils/isEmptyObject';
 
 export default function useTextFieldFormAdaptor(useFormReturn: UseFormReturn) {
-  // useForm의 handleSubmit 코드를 그대로 가져옴
-  // useForm 내부에서 진행하는 validation과 validation 이후 error 상태를 삭제하는 코드 제거
-  const handleSubmit: HandleSubmitType = useCallback(
-    (onValid, onInvalid) => async e => {
-      const control = useFormReturn.control;
+  // formState에 에러가 존재하는 경우에 handleSubmit 진행되지 않도록 수정
+  const handleSubmit: HandleSubmitType = (onValid, onInvalid) => async e => {
+    if (e) {
+      e.preventDefault && e.preventDefault();
+      e.persist && e.persist();
+    }
+    const originHandleSubmit = useFormReturn.handleSubmit;
+    const control = useFormReturn.control;
+    const _formState = control._formState;
 
-      if (e) {
-        e.preventDefault && e.preventDefault();
-        e.persist && e.persist();
+    // useForm의 validation 진행
+    control._updateValid(true);
+
+    // TextField가 이전에 에러 설정해둔 경우 false로 작동
+    if (isEmptyObject(_formState.errors)) {
+      return await originHandleSubmit(onValid, onInvalid)(e);
+    } else {
+      if (onInvalid) {
+        await onInvalid({ ..._formState.errors }, e);
       }
-
-      const fieldValues = { ...control._formValues };
-      const _subjects = control._subjects;
-      const _formState = control._formState;
-
-      _subjects.state.next({
-        isSubmitting: true
-      });
-
-      if (isEmptyObject(_formState.errors)) {
-        _subjects.state.next({
-          errors: {}
-        });
-        await onValid(fieldValues as FieldValue<any>, e);
-      } else {
-        if (onInvalid) {
-          await onInvalid({ ..._formState.errors }, e);
-        }
-      }
-
-      _subjects.state.next({
-        isSubmitted: true,
-        isSubmitting: false,
-        isSubmitSuccessful: isEmptyObject(_formState.errors),
-        submitCount: _formState.submitCount + 1,
-        errors: _formState.errors
-      });
-    },
-    [useFormReturn]
-  );
+    }
+  };
 
   const handleError: HandleErrorType = handleErrorReturn => {
     if (handleErrorReturn.error) {
       useFormReturn.setError(handleErrorReturn.name, {
+        type: handleErrorReturn.type || 'custom',
         message: handleErrorReturn.message || ''
       });
     } else {
       useFormReturn.clearErrors(handleErrorReturn.name);
+      useFormReturn.control._subjects.state.next({
+        name: handleErrorReturn.name,
+        isValid: true,
+        errors: {}
+      });
     }
   };
 
-  return { handleSubmit, handleError };
+  const onChange: OnChangeType = name => e => {
+    useFormReturn.setValue(name, e.currentTarget.value);
+  };
+
+  return { handleSubmit, handleError, onChange };
 }
 
 type HandleSubmitType = UseFormHandleSubmit<FieldValue<any>>;
 
 type HandleErrorType = (e: {
+  type?: string | any;
   error: boolean;
   name: string;
   message?: string;
 }) => void;
+
+type OnChangeType = (
+  name: string
+) => (e: SyntheticEvent<HTMLTextAreaElement | HTMLInputElement>) => void;
