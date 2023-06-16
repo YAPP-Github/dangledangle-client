9ab03354b6c29b5205e0d1a3c19d712dd5a40282
Paperkeem <ahharlawhddl@gmail.com>
2023-06-17 01:14:09 +0900

    ✨ feat: 특수동물 카드 추가

---
+++ src/components/shelter-edit/AnimalCard/Animal.css.ts
@@ -0,0 +1,53 @@
+import { palette } from '@/styles/color';
+import { style } from '@vanilla-extract/css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  padding: '16px',
+  gap: '10px',
+  width: '90%',
+  height: '180px',
+  background: palette.white,
+  borderRadius: '8px',
+  margin: 5
+});
+
+export const aniCard = style({
+  display: 'flex',
+  alignItems: 'center',
+  columnGap: '12px',
+  width: '100%'
+});
+
+export const divider = style({
+  boxSizing: 'border-box',
+  width: '100%',
+  height: '0px',
+  border: `0.5px solid ${palette.gray200}`,
+  marginTop: '10px',
+  marginBottom: '10px'
+});
+
+export const textClamp = style({
+  display: '-webkit-box',
+  WebkitBoxOrient: 'vertical',
+  WebkitLineClamp: 2,
+  overflow: 'hidden'
+});
+
+export const buttonWarp = style({
+  display: 'flex',
+  width: '100%',
+  columnGap: '12px',
+  alignItems: 'end',
+  justifyContent: 'end',
+  marginTop: '10px'
+});
+
+export const button = style({
+  width: '61px',
+  height: '34px',
+  borderRadius: '4px'
+});
