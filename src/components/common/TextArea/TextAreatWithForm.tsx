433c71affc4212386833aa5cc752009d56c47216
Sangjun-man <molung@icloud.com>
2023-06-17 08:57:39 +0900

    🔨 fix: textFieldForm, TextFieldArea 무한 리랜더링 버그 수정

--- src/components/common/TextArea/TextAreatWithForm.tsx
+++ src/components/common/TextArea/TextAreatWithForm.tsx
@@ -1,8 +1,6 @@
 import { RegisterOptions, useFormContext } from 'react-hook-form';
 import TextArea, { TextAreaProps } from './TextArea';
 import useTextFieldFormAdaptor from '../TextField/hooks/useTextFieldFormAdaptor';
-import { useEffect, useState } from 'react';
-import { TextFieldStatus } from '../TextField/hooks/useTextFieldStatus';
 
 interface AddRegisterOptions {
   registerOptions?: RegisterOptions;
@@ -10,42 +8,17 @@ interface AddRegisterOptions {
 function TextAreaWithForm({ ...props }: TextAreaProps & AddRegisterOptions) {
   const methods = useFormContext();
 
-  const [status, setStatus] = useState<{
-    status: TextFieldStatus;
-    message: string;
-  }>({
-    status: props.status || 'default',
-    message: props.message || ''
-  });
-  const thisFieldError = methods.formState.errors?.[props.name];
   const { handleError, onChange } = useTextFieldFormAdaptor(methods);
 
-  useEffect(() => {
-    if (thisFieldError) {
-      setStatus({
-        status: 'error',
-        message: String(thisFieldError.message) || ''
-      });
-    } else {
-      setStatus({
-        status: 'active',
-        message: ''
-      });
-    }
-  }, [thisFieldError]);
+  const thisFieldError = methods.formState.errors?.[props.name];
 
-  useEffect(() => {
-    if (props.status) {
-      setStatus({
-        status: props.status,
-        message: props.message || ''
-      });
-    }
-  }, [props.status]);
+  if (thisFieldError) {
+    props.status = 'error';
+    props.message = String(thisFieldError.message) || '';
+  }
   return (
     <TextArea
       {...props}
-      {...status}
       {...methods.register(props.name, { ...props.registerOptions })}
       onChange={onChange(props.name)}
       handleError={handleError}
--- src/components/common/TextField/TextFieldWIthForm.tsx
+++ src/components/common/TextField/TextFieldWIthForm.tsx
@@ -1,51 +1,25 @@
 import { RegisterOptions, useFormContext } from 'react-hook-form';
 import TextField, { TextFieldProps } from './TextField';
 import useTextFieldFormAdaptor from './hooks/useTextFieldFormAdaptor';
-import { useEffect, useState } from 'react';
-import { TextFieldStatus } from './hooks/useTextFieldStatus';
 
 interface AddRegisterOptions {
   registerOptions?: RegisterOptions;
 }
 function TextFieldWithForm({ ...props }: TextFieldProps & AddRegisterOptions) {
   const methods = useFormContext();
-  const [status, setStatus] = useState<{
-    status: TextFieldStatus;
-    message: string;
-  }>({
-    status: props.status || 'default',
-    message: props.message || ''
-  });
-  const thisFieldError = methods.formState.errors?.[props.name];
+
   const { handleError, onChange } = useTextFieldFormAdaptor(methods);
 
-  useEffect(() => {
-    if (thisFieldError) {
-      setStatus({
-        status: 'error',
-        message: String(thisFieldError.message) || ''
-      });
-    } else {
-      setStatus({
-        status: 'active',
-        message: ''
-      });
-    }
-  }, [thisFieldError]);
+  const thisFieldError = methods.formState.errors?.[props.name];
 
-  useEffect(() => {
-    if (props.status) {
-      setStatus({
-        status: props.status,
-        message: props.message || ''
-      });
-    }
-  }, [props.status]);
+  if (thisFieldError) {
+    props.status = 'error';
+    props.message = String(thisFieldError.message) || '';
+  }
 
   return (
     <TextField
       {...props}
-      {...status}
       {...methods.register(props.name, { ...props.registerOptions })}
       onChange={onChange(props.name)}
       handleError={handleError}
