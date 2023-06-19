3bf699cf2c819861e59b3ec93065315fd4568e90
Sangjun-man <molung@icloud.com>
2023-06-19 21:40:38 +0900

    ✨ feat: section에 className 전달 가능하도록 수정

--- src/components/common/BottomSheet/BottomSheet.tsx
+++ src/components/common/BottomSheet/BottomSheet.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { defaultContent, overlay, panel, panelOpen } from './BottomSheet.css';
+import clsx from 'clsx';
 
 interface BottomSheetProps {
   /** bottom sheet status */
   isOpened: boolean;
   /** bottom sheet close func */
   onClose?: () => void;
+
+  className?: string;
   /** bottom sheet 요소 */
   children?: React.ReactNode;
 }
@@ -13,6 +16,7 @@ interface BottomSheetProps {
 export default function BottomSheet({
   isOpened = false,
   onClose,
+  className,
   children = <div className={defaultContent}>약관에 동의해주세요.</div>
 }: BottomSheetProps) {
   const handleOverlayClick = () => {
@@ -22,7 +26,9 @@ export default function BottomSheet({
   return (
     <>
       {isOpened && <div className={overlay} onClick={handleOverlayClick} />}
-      <section className={`${panel} ${isOpened ? panelOpen : ''}`}>
+      <section
+        className={clsx([className, `${panel} ${isOpened ? panelOpen : ''}`])}
+      >
         {children}
       </section>
     </>
