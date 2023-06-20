6b4ece2ef0d7e7efffd30a9eb4a6603eef6712a9
Paperkeem <ahharlawhddl@gmail.com>
2023-06-21 00:22:19 +0900

    🚀 update: validation 로직 생성 및 분리

--- src/app/shelter/register/page.tsx
+++ src/app/shelter/register/page.tsx
@@ -1,8 +1,16 @@
 'use client';
 
+import { signUpPayload } from '@/api/shelter/auth/sign-up';
+import useShelterRegister from '@/api/shelter/auth/useShelterRegister';
 import FormProvider from '@/components/common/FormProvider/FormProvider';
 import useFunnel, { StepsProps } from '@/hooks/useFunnel';
+import { headerState } from '@/store/header';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { usePathname, useRouter } from 'next/navigation';
+import { useLayoutEffect } from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useSetRecoilState } from 'recoil';
+import { registerValidation } from '../utils/shelterValidaion';
 import Account from './components/Account';
 import Address from './components/Address';
 import Description from './components/Description';
@@ -10,14 +18,6 @@ import Hp from './components/Hp';
 import Name from './components/Name';
 import SpecificAddress from './components/SpecificAddress';
 import Sure from './components/Sure';
-import { usePathname, useRouter } from 'next/navigation';
-import { useSetRecoilState } from 'recoil';
-import { headerState } from '@/store/header';
-import { useLayoutEffect } from 'react';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
-import useShelterRegister from '@/api/shelter/auth/useShelterRegister';
-import { signUpPayload } from '@/api/shelter/auth/sign-up';
 
 export interface onNextProps {
   onNext: VoidFunction;
@@ -27,43 +27,6 @@ export interface onNextProps {
 export interface signUpFormValue extends signUpPayload {
   passwordConfirm: string;
 }
-
-/**
- * TODO
- * validation 스케마가 임시로 작성되어 수정이 필요합니다.
- * 각 페이지별로 에러에 따른 다음 버튼 disabled 상태 연결이 필요합니다.
- *
- */
-const validation = yup.object().shape({
-  email: yup
-    .string()
-    .required('필수항목 입니다.')
-    .email('올바른 이메일 형식이 아닙니다.'),
-  password: yup
-    .string()
-    .required()
-    .min(8, '비밀번호가 너무 짧습니다. 8~15자로 입력해주세요.')
-    .matches(/(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])/)
-    .max(15, '비밀번호가 너무 짧습니다. 8~15자로 입력해주세요.'),
-  passwordConfirm: yup
-    .string()
-    .optional()
-    .oneOf([yup.ref('password'), undefined], '비밀번호가 일치하지 않습니다'),
-  name: yup.string().max(20).required(), //// step2
-  phoneNumber: yup.string().required(), //// step3
-  address: yup.object().shape({
-    address: yup.string().required(),
-    addressDetail: yup.string().required('필수항목 입니다.'),
-    postalCode: yup.string().required(),
-    latitude: yup.number().required(),
-    longitude: yup.number().required()
-  }),
-  // [`address[0].addressDetail`]: yup.string().required(), //step5
-  description: yup
-    .string()
-    .required()
-    .max(300, '입력 가능 글자수를 초과했어요,') ///step6
-});
 
 const Steps: StepsProps<onNextProps>[] = [
   {
@@ -117,7 +80,7 @@ export default function ShelterRegister() {
   const methods = useForm<signUpFormValue>({
     mode: 'all',
     reValidateMode: 'onChange',
-    resolver: yupResolver(validation)
+    resolver: yupResolver(registerValidation)
   });
 
   const { mutateAsync } = useShelterRegister();
---
+++ src/app/shelter/utils/shelterValidaion.ts
@@ -0,0 +1,132 @@
+import { isExist } from '@/api/shelter/auth/login';
+import { debounce } from 'lodash';
+import * as yup from 'yup';
+
+const isExistValidation = async (
+  value: string,
+  values: yup.TestContext<any>,
+  type: 'NAME' | 'EMAIL'
+) => {
+  try {
+    const res: Record<'isExist', boolean> = await isExist(value, type);
+    if ('isExist' in res) {
+      return res.isExist as boolean;
+    }
+  } catch (e) {
+    values.createError({ path: 'email' });
+    return false;
+  }
+};
+
+const debounceIsExistValidation = debounce(isExistValidation, 300);
+
+yup.setLocale({
+  mixed: {
+    default: '사용할 수 없는 값입니다.',
+    required: '필수 입력 항목입니다.',
+    oneOf: '다음 값 중 하나여야 합니다.: ${values}',
+    notOneOf: '다음 값 중 하나가 아니어야 합니다.: ${values}',
+    notType: function notType(_ref: any) {
+      const type = _ref.type;
+
+      let msg;
+      if (type === 'number') {
+        msg = '숫자만 입력 가능합니다.';
+      } else if (type === 'date') {
+        msg = '날짜 형식으로만 입력 가능합니다.';
+      } else if (type === 'object') {
+        msg = '옵션을 선택해주세요.';
+      } else {
+        msg = '해당 항목은 `' + type + '` 형식으로 입력해주세요.';
+      }
+
+      return msg;
+    },
+    defined: '정의되지 않았습니다.'
+  },
+  string: {
+    length: '${length}자로 입력해주세요.',
+    min: '${min}자 이상 입력바랍니다.',
+    max: '${max}자 이하로 입력바랍니다.'
+  }
+});
+
+export const loginValidation = yup.object().shape({
+  email: yup
+    .string()
+    .required('필수항목 입니다.')
+    .email('올바른 이메일 형식이 아닙니다.'),
+  password: yup
+    .string()
+    .required()
+    .min(8, '비밀번호가 너무 짧습니다. 8~15자로 입력해주세요.')
+    .matches(
+      /(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])/,
+      '영문, 숫자, 특수문자 2가지 조합 8~15자로 입력해주세요.'
+    )
+    .max(15, '비밀번호가 너무 깁니다. 8~15자로 입력해주세요.')
+});
+
+export const passWordFindValidation = yup.object().shape({
+  email: yup
+    .string()
+    .required('필수항목 입니다.')
+    .email('올바른 이메일 형식이 아닙니다.')
+    .test(
+      'verified',
+      '입력하신 이메일 계정이 없습니다. 다시 한번 확인해주세요.',
+      async (value, values) => {
+        const verified = await debounceIsExistValidation(
+          value as string,
+          values,
+          'EMAIL'
+        );
+        return verified as boolean;
+      }
+    )
+});
+
+export const registerValidation = yup.object({
+  email: yup.string().required().email('올바른 이메일 형식이 아닙니다.'),
+  password: yup
+    .string()
+    .required()
+    .min(8, '비밀번호가 너무 짧습니다. 8~15자로 입력해주세요.')
+    .matches(
+      /(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])/,
+      '영문, 숫자, 특수문자 2가지 조합 8~15자로 입력해주세요.'
+    )
+    .max(15, '비밀번호가 너무 깁니다. 8~15자로 입력해주세요.'),
+  passwordConfirm: yup
+    .string()
+    .optional()
+    .oneOf([yup.ref('password'), undefined], '비밀번호가 일치하지 않습니다.'),
+  name: yup
+    .string()
+    .max(20)
+    .required()
+    .test(
+      'no-emoji',
+      '이모티콘은 사용할 수 없습니다.',
+      (value = '') =>
+        !/(\p{Emoji_Presentation}|\p{Extended_Pictographic})/gu.test(value)
+    ),
+  phoneNumber: yup
+    .string()
+    .required()
+    .matches(
+      /^(01[0-9]{1}|(02|0[3-9]{2}))[0-9]{3,4}[0-9]{4}$/,
+      '전화번호 형식이 아닙니다.'
+    ),
+  address: yup.object().shape({
+    address: yup.string().required(),
+    addressDetail: yup.string().required('필수항목 입니다.'),
+    postalCode: yup.string().required(),
+    latitude: yup.number().required(),
+    longitude: yup.number().required()
+  }),
+  description: yup
+    .string()
+    .required()
+    .max(300, '입력 가능 글자수를 초과했어요.')
+});
