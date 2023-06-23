271976a586fd0f1fccd84b2944cac2df65b4b5a3
HaYeong <hy78733@gmail.com>
2023-06-23 15:53:15 +0900

    🚀 update: color pallette, bg color

--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -1,6 +1,8 @@
 import { globalStyle } from '@vanilla-extract/css';
 import 'vanilla-reset';
+import { palette } from './color';
 
+globalStyle('body', { backgroundColor: palette.background });
 globalStyle('ul, ol, li', { listStyle: 'none' });
 export const BREAK_POINT = 600;
 export const GLOBAL_PADDING_X = 20;
