3c1d3a96efeb44507cb854d8a7d1cebb50f60ad1
Paperkeem <ahharlawhddl@gmail.com>
2023-07-02 03:28:27 +0900

    ✨ feat: calendar 컴포넌트 추가

---
+++ src/components/common/Calendar/DangleCalendar.css.ts
@@ -0,0 +1,129 @@
+import { palette } from '@/styles/color';
+import { GLOBAL_PADDING_X } from '@/styles/global.css';
+import { globalStyle, style } from '@vanilla-extract/css';
+import { recipe } from '@vanilla-extract/recipes';
+
+export const calendar = style({
+  width: '100%',
+  maxWidth: '100%',
+  margin: 'auto',
+  backgroundColor: palette.white,
+  color: palette.gray900,
+  padding: '20px',
+  transform: `translateX(${-GLOBAL_PADDING_X}px)`
+});
+
+export const dotWrapper = style({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  position: 'absolute'
+});
+
+export const dot = recipe({
+  base: {
+    height: '4px',
+    width: '4px',
+    borderRadius: '4px',
+    display: 'flex',
+    marginTop: '20px'
+  },
+  variants: {
+    date: {
+      today: {
+        backgroundColor: palette.white
+      },
+      other: {
+        backgroundColor: palette.gray600
+      }
+    }
+  }
+});
+
+globalStyle(`${calendar} button`, {
+  width: '32px',
+  height: '32px',
+  borderRadius: '6px',
+  padding: '4px 6px 4px 6px',
+  textAlign: 'center'
+});
+
+globalStyle(`${calendar} button:enabled:hover`, {
+  cursor: 'pointer'
+});
+
+// 캘린더 인디케이터
+globalStyle(`${calendar} .react-calendar__navigation`, {
+  display: 'flex',
+  alignItems: 'center',
+  height: '44px',
+  padding: '0px 80px 0px 80px'
+});
+
+globalStyle(`${calendar} .react-calendar__navigation button`, {
+  fontSize: '14px',
+  fontFamily: 'Pretendard',
+  fontStyle: 'normal',
+  fontWeight: 700
+});
+
+// 요일 column
+globalStyle(`${calendar} .react-calendar__month-view__weekdays`, {
+  padding: '12px 12px 4px 12px',
+  fontSize: '12px',
+  fontFamily: 'Pretendard',
+  fontStyle: 'normal',
+  fontWeight: 500,
+  lineHeight: '14px',
+  color: '#6c6c6c',
+  textAlign: 'center'
+});
+
+// 날짜 container
+globalStyle(`${calendar} .react-calendar__month-view__days`, {
+  padding: '0px 12px 0px 12px'
+});
+
+globalStyle(`${calendar} .react-calendar__tile`, {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+
+  fontSize: '12px',
+  fontFamily: 'Pretendard',
+  fontStyle: 'normal',
+  fontWeight: 500,
+  lineHeight: '14px',
+  color: '#6c6c6c'
+});
+
+// 날짜 호버됐을 시
+globalStyle(`${calendar} .react-calendar__tile:enabled:hover`, {
+  background: palette.gray100,
+  color: palette.primary300,
+  borderRadius: '6px'
+});
+
+globalStyle(`${calendar} .react-calendar__month-view__days__day--weekend`, {
+  color: palette.error
+});
+
+globalStyle(
+  `${calendar} .react-calendar__month-view__days__day--neighboringMonth`,
+  {
+    color: palette.gray200
+  }
+);
+
+// 오늘 날짜
+globalStyle(`${calendar} .react-calendar__tile--now `, {
+  background: palette.gray900,
+  color: palette.white
+});
+
+// 선택한 날짜
+globalStyle(`${calendar} .react-calendar__tile--active:enabled:focus`, {
+  background: palette.gray200,
+  color: palette.gray900
+});
---
+++ src/components/common/Calendar/DangleCalendar.tsx
@@ -0,0 +1,66 @@
+import { NextIcon, PrevIcon } from '@/asset/icons/index.ts';
+import clsx from 'clsx';
+import moment from 'moment';
+import Calendar, { CalendarProps } from 'react-calendar';
+import './DangleCalendar.css';
+import * as styles from './DangleCalendar.css.ts';
+import { LooseValue, Value } from 'react-calendar/dist/cjs/shared/types';
+
+interface DangleCalendarProps
+  extends Omit<CalendarProps, 'value' | 'onChange'> {
+  value?: LooseValue;
+  onChange?: (value: Value, event: React.MouseEvent<HTMLButtonElement>) => void;
+  mark?: string[];
+  changeMonthCallback?: (nextYear: number, nextMonth: number) => void;
+}
+
+export default function DangleCalendar({
+  value,
+  onChange,
+  mark,
+  changeMonthCallback,
+  ...rest
+}: DangleCalendarProps) {
+  return (
+    <>
+      <Calendar
+        {...rest}
+        value={value}
+        onChange={onChange}
+        className={styles.calendar}
+        locale="ko-KO"
+        formatDay={(locale, date) => moment(date).format('DD')}
+        next2Label={null}
+        prev2Label={null}
+        nextLabel={<NextIcon />}
+        prevLabel={<PrevIcon />}
+        onActiveStartDateChange={({ activeStartDate, value, view }) => {
+          const nextYear = activeStartDate!.getFullYear();
+          const nextMonth = activeStartDate!.getMonth() + 1;
+          changeMonthCallback?.(nextYear, nextMonth);
+        }}
+        tileContent={({ date, view }) => {
+          const html = [];
+          const today = moment().startOf('day');
+          const isToday = today.isSame(date, 'day');
+
+          if (mark?.find(x => x === moment(date).format('YYYY-MM-DD'))) {
+            html.push(
+              <div
+                key={Date.now()}
+                className={clsx([
+                  styles.dot({ date: isToday ? 'today' : 'other' })
+                ])}
+              ></div>
+            );
+          }
+          return (
+            <>
+              <div className={styles.dotWrapper}>{html}</div>
+            </>
+          );
+        }}
+      />
+    </>
+  );
+}
