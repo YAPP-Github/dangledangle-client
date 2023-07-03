3091564970041d185be065d8792d742af2ae258c
Paperkeem <ahharlawhddl@gmail.com>
2023-07-04 00:10:16 +0900

    🚀 update: volunteerEvent responseBody key 반영

--- src/components/common/Calendar/Calendar.tsx
+++ src/components/common/Calendar/Calendar.tsx
@@ -36,7 +36,7 @@ export default function Calendar() {
       />
 
       {eventMock?.map(event => (
-        <VolunteerEventCard key={event.eventId} event={event} />
+        <VolunteerEventCard key={event.volunteerEventId} event={event} />
       ))}
     </div>
   );
@@ -44,47 +44,55 @@ export default function Calendar() {
 
 const eventMock: VolunteerEvent[] = [
   {
+    eventStatus: 'DONE',
     category: '산책 봉사',
-    eventId: 11,
-    eventName: '한강 인근 댕댕이 산책 봉사자 모집합니다.',
-    participant: 7,
-    capacity: 5,
+    volunteerEventId: 11,
+    title: '한강 인근 댕댕이 산책 봉사자 모집합니다.',
+    recruitNum: 5,
+    participantNum: 5,
+    waitingNum: 2,
     date: '2023-07-01',
     startTime: '17:00',
     endTime: '19:00',
-    status: 'NONE'
+    myParticipationStatus: 'NONE'
   },
   {
+    eventStatus: 'DONE',
     category: '산책 봉사',
-    eventId: 12,
-    eventName: '태평역 인근 산책 봉사자 모집합니다.',
-    participant: 10,
-    capacity: 5,
+    volunteerEventId: 12,
+    title: '태평역 인근 산책 봉사자 모집합니다.',
+    recruitNum: 5,
+    participantNum: 5,
+    waitingNum: 5,
     date: '2023-07-02',
     startTime: '17:00',
     endTime: '18:00',
-    status: 'APPLY'
+    myParticipationStatus: 'PARTICIPATING'
   },
   {
+    eventStatus: 'IN_PROGRESS',
     category: '견사 청소',
-    eventId: 13,
-    eventName: '노원역 인근 견사 청소 봉사자 모집합니다.',
-    participant: 4,
-    capacity: 5,
+    volunteerEventId: 13,
+    title: '노원역 인근 견사 청소 봉사자 모집합니다.',
+    participantNum: 4,
+    recruitNum: 5,
+    waitingNum: 0,
     date: '2023-07-22',
     startTime: '17:00',
     endTime: '18:30',
-    status: 'WAITING'
+    myParticipationStatus: 'WAITING'
   },
   {
+    eventStatus: 'IN_PROGRESS',
     category: '홍보물 제작',
-    eventId: 14,
-    eventName: '홍보물 제작 봉사자 모집합니다.',
-    participant: 9,
-    capacity: 6,
+    volunteerEventId: 14,
+    title: '홍보물 제작 봉사자 모집합니다.',
+    recruitNum: 6,
+    participantNum: 6,
+    waitingNum: 3,
     date: '2023-07-25',
     startTime: '17:00',
     endTime: '19:30',
-    status: 'NONE'
+    myParticipationStatus: 'NONE'
   }
 ];
--- src/components/global/VolunteerEventCard/VolunteerEventCard.tsx
+++ src/components/global/VolunteerEventCard/VolunteerEventCard.tsx
@@ -12,15 +12,17 @@ import { usePathname } from 'next/navigation';
 import * as styles from './VolunteerEventCard.css';
 
 export interface VolunteerEvent {
+  eventStatus: 'IN_PROGRESS' | 'DONE' | 'CANCELED';
   category: string;
-  eventId: number;
-  eventName: string;
-  participant: number;
-  capacity: number;
+  volunteerEventId: number;
+  title: string;
+  recruitNum: number;
+  participantNum: number;
+  waitingNum: number;
   date: string | Date;
   startTime: string | Date;
   endTime: string | Date;
-  status?: 'APPLY' | 'WAITING' | 'NONE';
+  myParticipationStatus?: 'PARTICIPATING' | 'WAITING' | 'NONE';
 }
 
 interface VolunteerEventCardProps {
@@ -32,15 +34,17 @@ export default function VolunteerEventCard({ event }: VolunteerEventCardProps) {
   if (!event) return null;
 
   const {
+    eventStatus,
     category,
-    eventId,
-    eventName,
-    participant,
-    capacity,
+    volunteerEventId,
+    title,
+    recruitNum,
+    participantNum,
+    waitingNum,
     date,
     startTime,
     endTime,
-    status
+    myParticipationStatus
   } = event;
 
   return (
@@ -48,10 +52,10 @@ export default function VolunteerEventCard({ event }: VolunteerEventCardProps) {
       <H3 style={{ margin: '16px 0px 12px 0px' }}>{formatDate(date)}</H3>
       <div
         className={styles.wrapper({
-          status: isDatePast(date) ? 'done' : 'process'
+          status: eventStatus === 'IN_PROGRESS' ? 'process' : 'done'
         })}
       >
-        <Link href={`${pathname}/${eventId}`}>
+        <Link href={`${pathname}/${volunteerEventId}`}>
           <div className={styles.container}>
             <div className={styles.badgeWrapper}>
               {isDatePast(date) ? (
@@ -63,7 +67,7 @@ export default function VolunteerEventCard({ event }: VolunteerEventCardProps) {
               <Badge type="line">{`#${category}`}</Badge>
             </div>
 
-            <H4 className={styles.textClamp}>{eventName}</H4>
+            <H4 className={styles.textClamp}>{title}</H4>
 
             <div className={styles.infoContainer}>
               <div className={styles.infoWrapper}>
@@ -80,17 +84,15 @@ export default function VolunteerEventCard({ event }: VolunteerEventCardProps) {
               <div className={styles.infoWrapper}>
                 <Profile />
                 <Caption2 color="gray700">
-                  {participant > capacity ? capacity : participant}/{capacity}명
-                  {participant > capacity
-                    ? ` (대기 ${participant - capacity}명)`
-                    : null}
+                  {participantNum}/{recruitNum}명
+                  {waitingNum > 0 && `(대기 ${waitingNum}명)`}
                 </Caption2>
               </div>
 
               <div className={styles.status}>
-                {status === 'APPLY' ? (
+                {myParticipationStatus === 'PARTICIPATING' ? (
                   <Caption2 color="error">신청 완료</Caption2>
-                ) : status === 'WAITING' ? (
+                ) : myParticipationStatus === 'WAITING' ? (
                   <Caption2 color="gray600">신청 대기중</Caption2>
                 ) : null}
               </div>
