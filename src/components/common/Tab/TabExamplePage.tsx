b2674447f0f5104fea2b5cfb6b050ff6fc7f486e
Paperkeem <ahharlawhddl@gmail.com>
2023-07-05 16:59:04 +0900

    🪄 create: tab 컴포넌트 예시 페이지

--- src/components/common/Tab/TabExamplePage.tsx
+++ src/components/common/Tab/TabExamplePage.tsx
@@ -1,25 +1,27 @@
-import { TabPanel, Tab, TabList, Tabs } from '@/components/common/Tab';
+'use client';
+import Tabs from '@/components/common/Tab';
+import Calendar from '../Calendar/Calendar';
 
 interface Props {}
 
 export default function TabExamplePage({}: Props) {
   return (
     <Tabs defaultValue={0}>
-      <TabList>
-        <Tab value={0} title="0번째"></Tab>
-        <Tab value={1} title="1번째"></Tab>
-        <Tab value={2} title="2번째"></Tab>
-      </TabList>
+      <Tabs.TabList>
+        <Tabs.Tab value={0} title="봉사 모집 일정"></Tabs.Tab>
+        <Tabs.Tab value={1} title="안내 및 상세 정보"></Tabs.Tab>
+        <Tabs.Tab value={2} title="특별 케어 동물"></Tabs.Tab>
+      </Tabs.TabList>
 
-      <TabPanel value={0}>
-        <div>종이0</div>
-      </TabPanel>
-      <TabPanel value={1}>
-        <div>종이1</div>
-      </TabPanel>
-      <TabPanel value={2}>
-        <div>종이2</div>
-      </TabPanel>
+      <Tabs.TabPanel value={0}>
+        <Calendar />
+      </Tabs.TabPanel>
+      <Tabs.TabPanel value={1}>
+        <div>안내 및 상세 정보</div>
+      </Tabs.TabPanel>
+      <Tabs.TabPanel value={2}>
+        <div>특별 케어 동물</div>
+      </Tabs.TabPanel>
     </Tabs>
   );
 }
