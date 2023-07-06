c9b0b05cc3884b79a76e019f913dbafa67c1f07b
Paperkeem <ahharlawhddl@gmail.com>
2023-07-06 16:39:55 +0900

    🚀 update: map Component 가장 가까운 구청 or 시청 찾아서 return 로직 추가

--- src/components/global/Map/Map.tsx
+++ src/components/global/Map/Map.tsx
@@ -1,49 +1,21 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import { Body2 } from '@/components/common/Typography';
 import * as styles from './Map.css';
-
-declare global {
-  interface Window {
-    kakao: any;
-  }
-}
+import useMap from './hooks/useMap';
 
 interface MapProps {
   latitude: number;
   longitude: number;
+  address?: string;
 }
 
-export default function Map({ latitude, longitude }: MapProps) {
-  useEffect(() => {
-    const mapScript = document.createElement('script');
-
-    mapScript.async = true;
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API}&autoload=false`;
-
-    document.head.appendChild(mapScript);
-
-    const onLoadKakaoMap = () => {
-      window.kakao.maps.load(() => {
-        const container = document.getElementById('map');
-        const options = {
-          center: new window.kakao.maps.LatLng(latitude, longitude)
-        };
-        const map = new window.kakao.maps.Map(container, options);
-        const markerPosition = new window.kakao.maps.LatLng(
-          latitude,
-          longitude
-        );
-        const marker = new window.kakao.maps.Marker({
-          position: markerPosition
-        });
-        marker.setMap(map);
-      });
-    };
-    mapScript.addEventListener('load', onLoadKakaoMap);
-
-    return () => mapScript.removeEventListener('load', onLoadKakaoMap);
-  }, [latitude, longitude]);
-
-  return <div className={styles.mapContainer} id="map"></div>;
+export default function Map({ latitude, longitude, address }: MapProps) {
+  const { districtName } = useMap(longitude, latitude);
+  return (
+    <>
+      <Body2>{address || districtName}</Body2>
+      <div className={styles.mapContainer} id="map"></div>
+    </>
+  );
 }
