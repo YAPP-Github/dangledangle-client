40d61df88ad2f41393bd5e24184c50d2812d7f2b
Sangjun-man <molung@icloud.com>
2023-07-23 11:17:08 +0900

    🔨 fix: 헤더배경색상 변경
    - 헤더가 state로 관리되어, hydration 이후에야 타이틀과 색상이 변하는데,
      3차스프린트 이후 개선할 예정

--- src/components/common/ContainerWithStickyHeader/ContainerWithStickyHeader.tsx
+++ src/components/common/ContainerWithStickyHeader/ContainerWithStickyHeader.tsx
@@ -25,6 +25,7 @@ export default function ContainerWithStickyHeader({
 }: React.PropsWithChildren<StickyTitleProps>) {
   const setHeader = useHeader({
     ...headerProps,
+    color: 'white',
     RightSideComponent: ShareButton
   });
 
