2d53d742e8adc8992b84cf37226acaad7af66527
Paperkeem <ahharlawhddl@gmail.com>
2023-07-26 16:56:16 +0900

    🚀 update: filterBottom 분리 및 예시페이지 생성

--- src/components/common/Filter/Filter.css.ts
+++ src/components/common/Filter/Filter.css.ts
@@ -1,7 +1,5 @@
 import { palette } from '@/styles/color';
-import { BREAK_POINT, GLOBAL_PADDING_X } from '@/styles/global.css';
 import { style } from '@vanilla-extract/css';
-import { recipe } from '@vanilla-extract/recipes';
 
 export const container = style({
   display: 'inline-flex',
@@ -9,38 +7,13 @@ export const container = style({
   padding: '6px 8px 6px 12px',
   borderRadius: '35px',
   border: `1px solid ${palette.gray800}`,
-  cursor: 'pointer'
+  cursor: 'pointer',
+  ':hover': {
+    backgroundColor: palette.gray200
+  }
 });
 
 export const grid = style({
   display: 'flex',
   gap: '4px'
-});
-export const sheet = style({
-  backgroundColor: `${palette.white} !important`
-});
-export const sheetContainer = style({
-  padding: '0 24px 24px 24px',
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center'
-});
-export const label = style({
-  padding: '9px 248px 9px 0px',
-  width: `calc(100% - ${GLOBAL_PADDING_X}px)`,
-  maxWidth: BREAK_POINT,
-  borderBottom: `1px solid ${palette.gray100}`
-});
-export const labelTxt = recipe({
-  base: {},
-  variants: {
-    color: {
-      pick: {
-        color: palette.gray900
-      },
-      option: {
-        color: palette.gray400
-      }
-    }
-  }
 });
--- src/components/common/Filter/Filter.tsx
+++ src/components/common/Filter/Filter.tsx
@@ -1,70 +1,73 @@
 'use client';
 import { DropArrow } from '@/asset/icons';
 import useBooleanState from '@/hooks/useBooleanState';
-import { useState } from 'react';
-import BottomSheet from '../BottomSheet/BottomSheet';
-import { Body3, Body4, Caption3 } from '../Typography';
+import { useCallback, useState } from 'react';
+import { Caption3 } from '../Typography';
 import * as styles from './Filter.css';
-
-interface FilterProps {
+import FilterBottom from './FilterBottom';
+export interface FilterOption {
   label: string;
+  value: string;
+}
+interface FilterProps {
+  name: string;
+  label: string;
+  options: FilterOption[];
+  onChange: (name: string, value: string) => void;
 }
 
-const option = [
-  { label: '전체', value: 'ALL' },
-  { label: '모집중', value: 'IN_PROGRESS' },
-  { label: '모집 종료', value: 'DONE' }
-];
+/**
+ * `Filter` 컴포넌트는 필터 옵션을 선택할 수 있는 바텀시트를 제공합니다.
+ * 선택한 필터 옵션 값에 따라 처리 로직을 수행하려면 `onChange` prop에 콜백 함수를 전달해야 합니다.
+ *
+ * @component
+ * @example
+ * <Filter
+ *   name="region"
+ *   label="지역"
+ *   options={option_region}
+ *   onChange={handleSearchData}
+ * />
+ * @param name 필터에서 선택한 옵션 값을 처리하는 onChange 로직에서 구분자 역할을 합니다.
+ * @param label 필터 UI에 표시되는 텍스트입니다.
+ * @param options 선택할 수 있는 필터 옵션들의 배열입니다.
+ * @param onChange 필터 값을 변경할 때 호출되는 콜백 함수입니다. 필터 name과 선택된 option의 value 값이 인자로 전달됩니다.
+ */
 
-export default function Filter({ label }: FilterProps) {
+const Filter = ({ name, label, options, onChange }: FilterProps) => {
   const [isFilter, openFilter, closeFilter] = useBooleanState();
-  const [searchData, setSearchData] = useState(option[0].label);
+  const [pickOption, setPickOption] = useState(options[0]?.label);
+
+  const handleChangeData = useCallback(
+    (label: string, value: string) => {
+      setPickOption(label);
+      onChange(name, value);
+      closeFilter();
+    },
+    [name, onChange, closeFilter]
+  );
 
   return (
     <>
-      <button
-        className={styles.container}
-        onClick={() => {
-          openFilter();
-        }}
-      >
+      <button className={styles.container} onClick={openFilter}>
         <div className={styles.grid}>
           <Caption3>
-            {label} · {searchData}
+            {label} · {pickOption}
           </Caption3>
           <DropArrow />
         </div>
       </button>
 
-      <BottomSheet
-        isOpened={isFilter}
+      <FilterBottom
+        open={isFilter}
         onClose={closeFilter}
-        className={styles.sheet}
-      >
-        <section className={styles.sheetContainer}>
-          <Body3 style={{ textAlign: 'center' }}>{label}</Body3>
-          <ul>
-            {option?.map(({ label, value }) => (
-              <li
-                key={value}
-                className={styles.label}
-                onClick={() => {
-                  setSearchData(label);
-                  closeFilter();
-                }}
-              >
-                <Body4
-                  className={styles.labelTxt({
-                    color: label === searchData ? 'pick' : 'option'
-                  })}
-                >
-                  {label}
-                </Body4>
-              </li>
-            ))}
-          </ul>
-        </section>
-      </BottomSheet>
+        label={label}
+        options={options}
+        pickOption={pickOption}
+        onClick={handleChangeData}
+      />
     </>
   );
-}
+};
+
+export default Filter;
