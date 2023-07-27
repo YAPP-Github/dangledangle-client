d4db19eb3ea65ca6fb43c8b690072bbceb1bb766
Paperkeem <ahharlawhddl@gmail.com>
2023-07-27 11:23:09 +0900

    🚀 update: uuid 로직 변경

--- src/utils/uuidv4.ts
+++ src/utils/uuidv4.ts
@@ -1,6 +1,12 @@
 export default function uuidv4() {
+  const getRandomValue = () => {
+    const array = new Uint8Array(1);
+    crypto.getRandomValues(array);
+    return array[0] % 16;
+  };
+
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    const r = (Math.random() * 16) | 0,
+    const r = getRandomValue(),
       v = c === 'x' ? r : (r & 0x3) | 0x8;
     return v.toString(16);
   });
