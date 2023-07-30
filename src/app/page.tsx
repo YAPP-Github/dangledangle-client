3497dd0e5444cd5e342382950728a00f82f985b2
SangJun Lee <molung@icloud.com>
2023-07-30 16:42:51 +0900

    ✨ feat: 메인페이지 배너 ui 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+import Banner from '@/components/home/Banner/Banner';
 import HomeCalendar from '@/components/home/HomeCalendar/HomeCalendar';
 
 export default function HomePage() {
-  return <HomeCalendar />;
+  return (
+    <>
+      <Banner name="sangjun" shelterId="2" />
+      <HomeCalendar />
+    </>
+  );
 }
