af27ec4a956d249188a4982494f4e0166bb90f09
SangJun Lee <molung@icloud.com>
2023-07-31 04:34:46 +0900

    ✨ feat: 메인페이지 배너에 보호소 이름 띄워지도록 api 연동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,39 @@
+import * as volunteer from '@/api/volunteer/my';
+import * as shelter from '@/api/shelter/{shelterId}';
 import Banner from '@/components/home/Banner/Banner';
 import HomeCalendar from '@/components/home/HomeCalendar/HomeCalendar';
 import UpcommingScheduleSection from '@/components/home/UpcommingSchedule/UpcommingScheduleSection';
+import { COOKIE_ACCESS_TOKEN_KEY } from '@/constants/cookieKeys';
+import decodeDangleToken from '@/utils/token/decodeDangleToken';
+import { cookies } from 'next/headers';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const accessToken = cookies().get(COOKIE_ACCESS_TOKEN_KEY)?.value || '';
+  const { dangle_id, dangle_role: role } = decodeDangleToken(accessToken);
+
+  let name = '';
+  let shelterId = '';
+
+  try {
+    if (role === 'SHELTER' && dangle_id) {
+      const { name: shelterName } = await shelter.get(dangle_id);
+      name = String(shelterName);
+      shelterId = String(dangle_id);
+    }
+
+    //TODO : 봉사자 정보 요청에는 권한 필요, 서버쪽에서 어떻게 권한처리 할것인지?
+    // if (role === 'VOLUNTEER') {
+    //   const { nickName: volunteerName } = await volunteer.get();
+    //   console.log(volunteerName);
+    //   name = String(volunteerName);
+    // }
+  } catch (e) {
+    console.log(e);
+  }
+
   return (
     <>
-      <Banner name="sangjun" shelterId="2" />
+      <Banner name={name} shelterId={shelterId} />
       <UpcommingScheduleSection />
       <HomeCalendar />
     </>
