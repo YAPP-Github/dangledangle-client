7c7ccf38a42c192159148e80a99342227a51b0b1
Paperkeem <ahharlawhddl@gmail.com>
2023-08-02 17:03:29 +0900

    🚀 update: 미들웨어 라우팅 (in progress)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,18 @@
-import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import { COOKIE_ACCESS_TOKEN_KEY } from './constants/cookieKeys';
 import appendHeaderTitle from './utils/middleware/hooks/appendHeaderProps';
 
 export async function middleware(req: NextRequest) {
-  const requestHeaders = new Headers(req.headers);
+  const accessToken = req.cookies.get(COOKIE_ACCESS_TOKEN_KEY)?.value || '';
 
+  if (!accessToken) {
+    if (req.nextUrl.pathname.startsWith('/admin')) {
+      return NextResponse.redirect(new URL('/login', req.url));
+    }
+  }
+
+  const requestHeaders = new Headers(req.headers);
   appendHeaderTitle({ req, requestHeaders });
 
   return NextResponse.next({
