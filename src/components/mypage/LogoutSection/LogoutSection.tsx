625820623378b120704679ced1873c6bd1e1d587
Paperkeem <ahharlawhddl@gmail.com>
2023-08-02 10:31:19 +0900

    🚀 update: 로그아웃 모달 업데이트

--- src/components/mypage/LogoutSection/LogoutSection.tsx
+++ src/components/mypage/LogoutSection/LogoutSection.tsx
@@ -2,12 +2,26 @@
 import { Body1, ButtonText2 } from '@/components/common/Typography';
 import * as styles from './LogoutSection.css';
 import useLogout from '@/api/mypage/useLogout';
+import useDialog from '@/hooks/useDialog';
 
 interface LogoutSectionProps {}
 
 export default function LogoutSection({}: LogoutSectionProps) {
   const { mutate: logout } = useLogout();
-  const handleLogout = async () => logout();
+  const { dialogOn, dialogOff } = useDialog();
+  const handleLogout = async () => {
+    dialogOn({
+      message: '로그아웃 하시겠습니까?',
+      close: {},
+      confirm: {
+        text: '로그아웃',
+        onClick: () => {
+          dialogOff();
+          logout();
+        }
+      }
+    });
+  };
 
   return (
     <div className={styles.container}>
