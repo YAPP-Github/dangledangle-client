246850e35b3f67342074a4c5089a1ab92b6e6a3b
SangJun Lee <molung@icloud.com>
2023-08-05 05:45:58 +0900

    🔨 fix: main layout의 margin-collapse 현상 수정

--- src/app/layout.css.ts
+++ src/app/layout.css.ts
@@ -16,6 +16,7 @@ export const container = style({
 });
 
 export const main = style({
+  overflow: 'auto',
   padding: `0 ${GLOBAL_PADDING_X}px`,
   minHeight: `calc(100vh - ${FOOTER_HEIGHT}px - ${HEADER_HEIGHT}px)`
 });
