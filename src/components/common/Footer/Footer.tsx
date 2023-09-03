2ee2b7991f58415be32ea1d4c988219daa029f2b
SangJun Lee <molung@icloud.com>
2023-09-03 20:34:55 +0900

    ✨ feat: footer의 서비스 이용약관 링크 연동

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -8,6 +8,7 @@ import {
 } from '@/utils/middleware/footerServerSideRenderProp';
 import { usePathname } from 'next/navigation';
 import { matchURL } from '@/utils/middleware/matchUrl';
+import { MouseEventHandler, useCallback } from 'react';
 
 type FooterProps = Omit<FooterServerSideRenderProp, 'url'>;
 
@@ -17,13 +18,22 @@ export default function Footer({ backgroundColor = 'default' }: FooterProps) {
   const matchedIndex = matchedFooterURL(path);
   const visiblity = matchedIndex !== null;
 
+  const handleClick: (href: string) => MouseEventHandler<HTMLDivElement> =
+    useCallback(
+      href => e => {
+        e.preventDefault();
+        window.open(href);
+      },
+      []
+    );
+
   const footerLinks = [
     {
       href: '#',
       title: '개인정보 처리방침'
     },
     {
-      href: '#',
+      href: 'https://dankim9494.notion.site/dankim9494/8334ce2511894b71a0f57dca52367e4c',
       title: '이용약관'
     },
     {
@@ -46,10 +56,8 @@ export default function Footer({ backgroundColor = 'default' }: FooterProps) {
           </div>
           <div className={styles.linkWrapper}>
             {footerLinks.map(({ href, title }, index) => (
-              <div key={`footer_${index}`}>
-                <a href={href}>
-                  <Caption1>{title}</Caption1>
-                </a>
+              <div key={`footer_${index}`} onClick={handleClick(href)}>
+                <Caption1>{title}</Caption1>
                 {index < footerLinks.length - 1 && <Caption1>•</Caption1>}
               </div>
             ))}
